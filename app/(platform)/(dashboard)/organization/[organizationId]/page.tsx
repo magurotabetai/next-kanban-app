454c1e6246e5751ab96844d8c37c27ab4d633c67
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2023-12-06 21:08:36 +0900

    Fix form validation and error handling in create-board action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,24 +1,13 @@
 import prisma from "@/lib/prisma";
-import { create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
 import { Board } from "./board";
+import { Form } from "./form";
 
 const OrganizationPage = async () => {
   const boards = await prisma.board.findMany();
 
   return (
     <div className="flex flex-col space-y-4">
-      <form action={create}>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          required
-          placeholder="Enter a board title"
-          className="border-black border p-1"
-        />
-        <Button type="submit">Create</Button>
-      </form>
+      <Form />
       <div className="space-y-2">
         {boards.map((board) => (
           <Board key={board.id} title={board.title} id={board.id} />
