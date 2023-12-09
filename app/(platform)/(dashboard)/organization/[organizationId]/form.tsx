1504e6b365c3968469cca8217635597c94b2701f
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2023-12-09 15:45:11 +0900

    Remove unused FormButton component and replace with FormSubmit

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { createBoard } from "@/actions/create-board";
-import { FormButton } from "./form-button";
+// import { FormButton } from "./form-button";
+import { FormSubmit } from "@/components/form/form-submit";
 import { useAction } from "@/hooks/use-action";
 import { FormInput } from "@/components/form/form-input";
 
@@ -28,7 +29,7 @@ export const Form = () => {
       <div className="flex flex-col space-y-2">
         <FormInput label="Board Title" id="title" errors={fieldErrors} />
       </div>
-      <FormButton />
+      <FormSubmit>Save</FormSubmit>
     </form>
   );
 };
---
+++ components/form/form-submit.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+interface FormButtonProps {
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "primary"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "primary";
+}
+
+export const FormSubmit = ({
+  children,
+  disabled,
+  className,
+  variant,
+}: FormButtonProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      disabled={pending || disabled}
+      type="submit"
+      variant={variant}
+      size="sm"
+      className={cn(className)}
+    >
+      {children}
+    </Button>
+  );
+};
