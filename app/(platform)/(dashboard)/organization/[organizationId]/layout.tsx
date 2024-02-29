403d13c8a56c0de423d0050e26da1ddb12c4144c
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2024-02-29 17:27:27 +0900

    Add generateMetadata function to layout.tsx

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,13 @@
+import { auth } from "@clerk/nextjs";
 import { OrgControl } from "./_components/org-control";
+
+export const generateMetadata = async () => {
+  const { orgSlug } = auth();
+
+  return {
+    title: orgSlug || "organization",
+  };
+};
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
