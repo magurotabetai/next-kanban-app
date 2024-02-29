eeaecb6d8734f175141be2736c05036a72fe0b7e
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2024-02-29 22:02:02 +0900

    Add list components and page for board

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,3 +1,46 @@
-export default function Page() {
-  return <div className="">Board ID!</div>;
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
 }
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await prisma.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer data={lists} boardId={params.boardId} />
+      Board ID!
+    </div>
+  );
+};
+
+export default BoardIdPage;
