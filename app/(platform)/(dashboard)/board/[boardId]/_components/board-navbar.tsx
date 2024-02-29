e3d98830c88cbdd6b8541dadc7c8c600571a3f69
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2024-02-29 18:07:47 +0900

    Add board navbar component

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -0,0 +1,14 @@
+import { Board } from "@prisma/client";
+import { BoardTitleForm } from "./board-title-form";
+
+interface BoardNavBarProps {
+  data: Board;
+}
+
+export const BoardNavBar = async ({ data }: BoardNavBarProps) => {
+  return (
+    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
+      <BoardTitleForm data={data} />
+    </div>
+  );
+};
--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs";
 import prisma from "@/lib/prisma";
 import { notFound, redirect } from "next/navigation";
+import { BoardNavBar } from "./_components/board-navbar";
 
 export async function generateMetadata({
   params,
@@ -56,6 +57,8 @@ const BoardIdLayout = async ({
       className="relative h-full bg-no-repeat bg-cover bg-center"
       style={{ backgroundImage: `url(${board.imageFullUrl})` }}
     >
+      <BoardNavBar data={board} />
+      <div className="absolute inset-0 bg-black/10"></div>
       <main className="relative pt-28 h-full">{children}</main>
     </div>
   );
