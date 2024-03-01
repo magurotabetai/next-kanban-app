dcb208ed4b47d6b2ede02a13564faa3859b5f14a
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2024-03-01 12:46:05 +0900

    Add deleteList action and related files

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -11,6 +11,10 @@ import { Button } from "@/components/ui/button";
 import { MoreHorizontal, X } from "lucide-react";
 import { FormSubmit } from "@/components/form/form-submit";
 import { Separator } from "@/components/ui/separator";
+import { useAction } from "@/hooks/use-action";
+import { deleteList } from "@/actions/delete-list";
+import { toast } from "sonner";
+import { ElementRef, useRef } from "react";
 
 interface ListOptionsProps {
   data: ListWithCards;
@@ -18,6 +22,25 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+
+  const { execute: executeDelete } = useAction(deleteList, {
+    onSuccess: (data) => {
+      toast.success(`List ${data.title} deleted`);
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onDelete = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeDelete({ id, boardId });
+  };
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -29,7 +52,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           List Actions
         </div>
-        <PopoverClose asChild>
+        <PopoverClose asChild ref={closeRef}>
           <Button
             variant="ghost"
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
@@ -63,7 +86,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
 
         <Separator />
 
-        <form action="">
+        <form action={onDelete}>
           <input type="hidden" name="id" id="id" value={data.id} />
           <input
             type="hidden"
