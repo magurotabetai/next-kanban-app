65605aca13163052813881a847b3c9ef1e6516ff
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2024-03-05 22:03:01 +0900

    ローカルでドラッグアンドドロップ機能を追加

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -4,6 +4,7 @@ import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
 import { useEffect, useState } from "react";
 import { ListItem } from "./list-item";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 
 interface ListContainerProps {
   data: ListWithCards[];
@@ -18,12 +19,23 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   }, [data]);
 
   return (
-    <ol className="flex gap-x-3 h-full">
-      {orderedData.map((list, index) => {
-        return <ListItem key={list.id} data={list} index={index} />;
-      })}
-      <ListForm />
-      <div className="flex-shrink-0 w-1"></div>
-    </ol>
+    <DragDropContext onDragEnd={() => {}}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => {
+              return <ListItem key={list.id} data={list} index={index} />;
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1"></div>
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
