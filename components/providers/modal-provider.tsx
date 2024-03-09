05d2a04238ec6d418a0b7072502f58625a22968c
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2024-03-09 17:40:04 +0900

    Reactのimport文を修正しました

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CardModal } from "../modal/card-modal";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
