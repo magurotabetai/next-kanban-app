4033d474631311cafaa1e8f9f2105e5950ed6634
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2024-03-09 18:12:19 +0900

    createAuditLog関数を追加

---
+++ lib/create-audit-log.ts
@@ -0,0 +1,36 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+
+import prisma from "@/lib/prisma";
+
+interface Props {
+  entityId: string;
+  entityType: ENTITY_TYPE;
+  entityTitle: string;
+  action: ACTION;
+}
+
+export const createAuditLog = async (props: Props) => {
+  try {
+    const { orgId } = auth();
+    const user = await currentUser();
+
+    if (!orgId || !user) {
+      throw new Error("User not found");
+    }
+
+    const { entityId, entityType, entityTitle, action } = props;
+    await prisma.auditLog.create({
+      data: {
+        action,
+        entityType,
+        entityId,
+        entityTitle,
+        orgId,
+        userId: user.id,
+        userImage: user.imageUrl,
+        userName: user.firstName + " " + user.lastName,
+      },
+    });
+  } catch (error) {}
+};
