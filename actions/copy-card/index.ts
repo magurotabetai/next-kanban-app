9496b4194f84b37aa7d02e9fe6cb1cbd2e8d3925
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2024-03-09 17:44:47 +0900

    copyCardアクションを追加

---
+++ actions/copy-card/index.ts
@@ -0,0 +1,75 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs";
+import { InputType, ReturnType } from "./types";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { CopyCard } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id, boardId } = data;
+  let card;
+
+  try {
+    const cardToCopy = await prisma.card.findUnique({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+      include: {
+        list: true,
+      },
+    });
+
+    if (!cardToCopy) {
+      return {
+        error: "Card not found.",
+      };
+    }
+
+    const lastCard = await prisma.card.findFirst({
+      where: {
+        listId: cardToCopy.listId,
+      },
+      orderBy: {
+        order: "desc",
+      },
+      select: {
+        order: true,
+      },
+    });
+
+    const newOrder = lastCard ? lastCard.order + 1 : 1;
+
+    card = await prisma.card.create({
+      data: {
+        title: `${cardToCopy.title} - Copy`,
+        order: newOrder,
+        description: cardToCopy.description,
+        listId: cardToCopy.listId,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to copy.",
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const copyCard = createSafeAction(CopyCard, handler);
