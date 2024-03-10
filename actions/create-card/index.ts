992c776245f53917330f1e39cbb31207a778f5b6
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2024-03-10 17:30:18 +0900

    モーダルにアクティビティを表示

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -6,6 +6,8 @@ import { InputType, ReturnType } from "./types";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateCard } from "./schema";
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -50,6 +52,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         listId,
         order: newOrder,
       },
+    });
+
+    await createAuditLog({
+      entityId: card.id,
+      entityType: ENTITY_TYPE.CARD,
+      entityTitle: card.title,
+      action: ACTION.CREATE,
     });
   } catch (error) {
     return {
---
+++ app/api/cards/[cardId]/logs/route.ts
@@ -0,0 +1,33 @@
+import { auth } from "@clerk/nextjs";
+import prisma from "@/lib/prisma";
+import { ENTITY_TYPE } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { cardId: string } }
+) {
+  try {
+    const { userId, orgId } = auth();
+
+    if (!userId || !orgId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const auditLogs = await prisma.auditLog.findMany({
+      where: {
+        orgId,
+        entityId: params.cardId,
+        entityType: ENTITY_TYPE.CARD,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 3,
+    });
+
+    return NextResponse.json(auditLogs);
+  } catch (error) {
+    return new Response("Internal server error", { status: 500 });
+  }
+}
