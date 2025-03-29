2d51572588a833b5d75f5313366df57cea40800a
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2025-03-29 19:54:43 +0900

    chore: npx @tailwindcss/upgrade

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
-      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-xs bg-white flex items-center">
+      <div className="md:max-w-(--breakpoint-2xl) mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
           <Button size="sm" variant="outline" asChild>
