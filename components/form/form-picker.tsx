8804224e75210ab0558b656e21b58adc1acb1c34
magurotabetai <3165765+magurotabetai@users.noreply.github.com>
2025-03-29 20:28:57 +0900

    FormPickerコンポーネントのチェックボックスをdefaultCheckedに変更

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -75,7 +75,7 @@ export const FormPicker = ({ id, errors }: FormPickerProps) => {
               name={id}
               id={id}
               className="hidden"
-              checked={selectedImageId === image.id}
+              defaultChecked={selectedImageId === image.id}
               disabled={pending}
               value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
             />
